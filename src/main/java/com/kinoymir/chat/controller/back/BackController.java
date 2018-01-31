c8c154d3f9bd1c61385eda266c48bc557f1b3d6e
kinoymir <lywszh@vip.qq.com>
2018-01-31 14:45:54 +0800

    一些修改

--- src/main/java/com/kinoymir/chat/controller/back/BackController.java
+++ src/main/java/com/kinoymir/chat/controller/back/BackController.java
@@ -16,20 +16,47 @@ public class BackController extends BaseController {
 
     /**
      * 后台模板页面
+     *
      * @return 页面
      */
     @GetMapping("/template")
-    public String template(){
+    public String template() {
         return "template";
     }
 
     /**
+     * 后台的登录页面
+     *
+     * @return
+     */
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "登录");
+        return "login";
+    }
+
+    /**
      * 后台首页
+     *
      * @return 页面
      */
     @GetMapping("/index")
-    public String index(Model model){
-        model.addAttribute("title","首页");
+    public String index(Model model) {
+        model.addAttribute("title", "首页");
+        model.addAttribute("page", "index");
         return "index";
+    }
+
+    /**
+     * 用户管理页面
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/userManager")
+    public String userManager(Model model) {
+        model.addAttribute("title", "用户管理");
+        model.addAttribute("page", "userManager");
+        return "userManager";
     }
 }
