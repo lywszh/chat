5eb88bbf61b62ea87501c4aef30a2da49cf28eba
kinoymir <lywszh@vip.qq.com>
2018-02-25 15:56:04 +0800

    一些修改和新增

--- src/main/java/com/kinoymir/chat/controller/BaseController.java
+++ src/main/java/com/kinoymir/chat/controller/BaseController.java
@@ -1,9 +1,18 @@
 package com.kinoymir.chat.controller;
 
+import com.kinoymir.chat.common.ChatRuntimeException;
 import com.kinoymir.chat.config.shiro.MyShiroToken;
 import com.kinoymir.chat.entity.user.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseController {
 
@@ -32,5 +41,51 @@ public class BaseController {
         subject.login(token);
     }
 
+    /**
+     * 下载文件
+     *
+     * @param fsr
+     * @return
+     */
+    private ResponseEntity<InputStreamResource> downloadServerFile(FileSystemResource fsr) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", fsr.getFilename()));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        ResponseEntity<InputStreamResource> body = null;
+        try {
+            body = ResponseEntity.ok().headers(headers).contentLength(fsr.contentLength())
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .body(new InputStreamResource(fsr.getInputStream()));
+        } catch (IOException e) {
+            throw new ChatRuntimeException("传输错误");
+        }
+        return body;
+    }
+
+    /**
+     * 下载文件
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    protected ResponseEntity<InputStreamResource> downloadServerFile(String path) {
+        FileSystemResource fsr = new FileSystemResource(path);
+        return downloadServerFile(fsr);
+    }
+
+    /**
+     * 下载文件
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    protected ResponseEntity<InputStreamResource> downloadServerFile(File file) {
+        FileSystemResource fsr = new FileSystemResource(file);
+        return downloadServerFile(fsr);
+    }
 
 }
---
+++ src/main/java/com/kinoymir/chat/task/TmpClean.java
@@ -0,0 +1,66 @@
+package com.kinoymir.chat.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+/**
+ * 清理临时文件夹(定时任务，每小时执行一次)
+ *
+ * @author kino
+ *
+ */
+@Component
+public class TmpClean {
+
+    private Logger logger = LoggerFactory.getLogger(TmpClean.class);
+
+    /**
+     * 新文件创建保护时间
+     */
+    private static final int CLEAN_MINUTE = 15;
+
+    @Scheduled(cron = "0 0 0/1 * * ?")
+    public void tmpClean() {
+        logger.warn("开始清理临时文件夹!");
+        String tmpPath = "tmps/";
+        File file = new File(tmpPath);
+        try {
+            deleteFolder(file);
+        } catch (Exception e) {
+            logger.warn("清理临时文件夹失败");
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteFolder(File file) throws Exception {
+        if (file.isDirectory()) {
+            File[] fl = file.listFiles();
+            for (int i = 0; i < fl.length; i++) {
+                deleteFolder(fl[i]);
+            }
+            if (isFileOld(file)) {
+                logger.warn("删除文件夹 {}", file.getName());
+                // 暂时不删除文件夹
+                // file.delete();
+            }
+
+        } else {
+            if (isFileOld(file)) {
+                logger.warn("删除文件  {}", file.getName());
+                file.delete();
+            }
+        }
+    }
+
+    public boolean isFileOld(File file) {
+        long modifiedTime = file.lastModified();
+        long currentTime = System.currentTimeMillis();
+        // 设置最后一次修改的时间
+        long daysBetween = (currentTime - modifiedTime) / (1000 * 60);
+        return daysBetween >= CLEAN_MINUTE ? true : false;
+    }
+}
